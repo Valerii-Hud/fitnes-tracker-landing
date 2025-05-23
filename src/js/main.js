57d3b7be74e1f580a8be8f676b4aa182b4859bd4
Valerii Hud <support@valerii-hud.com>
2025-05-23 09:58:51 +0200

    feat: added about section with slick slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import '/src/less/main.less';
+
+$(document).ready(function () {
+  $('.about__slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    appendDots: $('.about__dots'),
+    customPaging: function (slider, i) {
+      return '<button class="about__dot"></button>';
+    },
+  });
+});
