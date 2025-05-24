c728cd56437a4cb575611ecdb56082af5591e7ce
Valerii Hud <support@valerii-hud.com>
2025-05-24 10:07:15 +0200

    feat: added tariff plan section

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,22 @@ $(document).ready(function () {
       return '<button class="about__dot"></button>';
     },
   });
+
+  // Логика табов
+  const tabs = document.querySelectorAll('.plan__tab');
+  const contents = document.querySelectorAll('.plan__content');
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      const target = tab.getAttribute('data-tab');
+
+      // Удаляем активные классы
+      tabs.forEach((t) => t.classList.remove('plan__tab_active'));
+      contents.forEach((c) => c.classList.remove('plan__content_active'));
+
+      // Добавляем активные классы для выбранных
+      tab.classList.add('plan__tab_active');
+      document.getElementById(target).classList.add('plan__content_active');
+    });
+  });
 });
